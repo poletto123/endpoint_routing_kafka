e9792b1f3514984455ee46e3e6818c8c436efe28
William Poletto <poletto123@gmail.com>
2021-02-26 17:25:02 -0300

    Project created

---
+++ src/main/java/com/williapoletto/springbootkafka/SpringBootWithKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.williapoletto.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williapoletto/springbootkafka/SpringBootWithKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williapoletto.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
